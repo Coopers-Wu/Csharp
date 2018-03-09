669595d62f53f56da73b4af4ddb123d7abfee398
cooperwu <269926183@qq.com>
2018-03-09 20:59:07 +0800

    create the world

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person per = new Person(5);
+            per.GrowUp();
+            per.Say();
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+    class Person
+    {
+        public Person(int a)
+        {
+            this.age = a;
+        }
+        int age;
+        public void GrowUp()
+        {
+            this.age++;
+        }
+
+        public void Say()
+        {
+            Console.WriteLine("my age is {0}",this.age);
+        }
+    }
+}
+ 
